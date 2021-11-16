55a4827d7de27811c43498e2d7ea289e06a29d8f
MrCardamon <cardamon.coc@gmail.com>
2021-11-16 11:37:11 +0100

    more basic shit

--- 0101main.py
+++ 0101main.py
@@ -1,2 +1,17 @@
 import numpy
 import torch
+
+x= torch.empty(2,2)
+print(x) 
+
+x= torch.ones(2,2, dtype = torch.float16)
+print(x.dtype)
+
+x= torch.rand(2,2)
+y= torch.rand(2,2)
+
+z= torch.add(x,y)
+print(z)
+
+y.add_(x) # any _ in torch is inplace
+print(y)
